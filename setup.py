8d4a614beaa6c2cf8fef29c7faafe40a4ac854de
chintuboi2 <bhoi.chintu@gmail.com>
2022-12-14 15:19:34 +0530

    update requirements files

--- setup.py
+++ setup.py
@@ -1,7 +1,15 @@
 from setuptools import find_packages,setup
+from typing import List
 
-def get_requirements():
-    pass
+REQUIREMENT_FILE_NAME = "requirements.txt"
+HYPHEN_E_DOT = "-e ."
+def get_requirements()->List[str]:
+    with open(REQUIREMENT_FILE_NAME) as requirement_files:
+        requirement_list = requirement_files.readline()
+    requirement_list = [requirement_list.replace("\n","") for requirement_name in requirement_list]
+    if HYPHEN_E_DOT in requirement_list:
+        requirement_list.remove(HYPHEN_E_DOT)
+    return requirement_files
 
 setup(
     name = "sensor",
@@ -10,6 +18,4 @@ setup(
     author_email = "bhoi.chintu@gmail.com",
     package = find_package(),
     install_requires = get_requirements(),
-
-
 )
