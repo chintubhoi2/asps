0ade50a000c4a002f084206e8ed80dd35f39bd1c
chintuboi2 <bhoi.chintu@gmail.com>
2022-12-14 17:09:33 +0530

    added folder structures

--- setup.py
+++ setup.py
@@ -3,19 +3,22 @@ from typing import List
 
 REQUIREMENT_FILE_NAME = "requirements.txt"
 HYPHEN_E_DOT = "-e ."
+
 def get_requirements()->List[str]:
-    with open(REQUIREMENT_FILE_NAME) as requirement_files:
-        requirement_list = requirement_files.readline()
-    requirement_list = [requirement_list.replace("\n","") for requirement_name in requirement_list]
+
+    with open(REQUIREMENT_FILE_NAME) as requirement_file:
+        requirement_list = requirement_file.readlines()
+    requirement_list = [requirement_name.replace("\n","") for requirement_name in requirement_list]
+    
     if HYPHEN_E_DOT in requirement_list:
         requirement_list.remove(HYPHEN_E_DOT)
-    return requirement_files
+    return requirement_list
 
 setup(
     name = "sensor",
     version = "0.0.1",
     author = "ved",
     author_email = "bhoi.chintu@gmail.com",
-    package = find_package(),
+    packages = find_packages(),
     install_requires = get_requirements(),
 )
